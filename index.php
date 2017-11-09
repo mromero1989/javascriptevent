52e9d2b02623ae3401bc44943f9bb82c571db6ab
Michael Romero <m.romero1989@gmail.com>
2017-11-09 13:37:10 -0700

    Fixed typo

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<script type="text/javascript" src="js/scripts.js"></script>
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
-		<!--links to bootstrap, jquery-->
+		<!--links to bootstrap, jquery yo-->
 
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
